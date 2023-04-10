d69437062c03f2601e7bdc75310f061ab8f4b7e3
Your Name <you@example.com>
2023-04-09 17:20:29 -0700

    finished modifying heex

--- lib/store_manager_web/live/clerk_live/index.ex
+++ lib/store_manager_web/live/clerk_live/index.ex
@@ -13,7 +13,7 @@ defmodule StoreManagerWeb.ClerkLive.Index do
   @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end <
+  end
 
   defp apply_action(socket, :edit, %{"id" => id}) do
     socket
